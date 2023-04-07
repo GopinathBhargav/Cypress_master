0c115d7d41b50aa16388e588f5f9ca737a504009
GopinathBhargav <40921394+GopinathBhargav@users.noreply.github.com>
2023-04-07 10:51:16 +0400

    commented the code for select file .js

--- cypress/e2e/advanced-examples/select-file.cy.js
+++ cypress/e2e/advanced-examples/select-file.cy.js
@@ -1,27 +1,27 @@
-describe("verify by selecting a file from project directory", () => {
+// describe("verify by selecting a file from project directory", () => {
 
-    it("select the png file from project repository", () => {
-
-
-        cy.fixture('test_approach.jpeg', { encoding: null }).as('myFixture')
-
-        cy.visit("http://www.webdriveruniversity.com/")
-
-        cy.get("#file-upload")
-            .scrollIntoView()
-            .invoke('removeAttr', 'target')
-            .click();
-
-        cy.get("input#myFile")
-            .selectFile('@myFixture')
-
-        cy.get("#submit-button")
-            .click()
-
-        cy.on('window:alert', (text) => {
-            expect(text).to.include('Your file has now been uploaded!')
-        })
+//     it("select the png file from project repository", () => {
 
 
-    })
-})
+//         cy.fixture('test_approach.jpeg', { encoding: null }).as('myFixture')
+
+//         cy.visit("http://www.webdriveruniversity.com/")
+
+//         cy.get("#file-upload")
+//             .scrollIntoView()
+//             .invoke('removeAttr', 'target')
+//             .click();
+
+//         cy.get("input#myFile")
+//             .selectFile('@myFixture')
+
+//         cy.get("#submit-button")
+//             .click()
+
+//         cy.on('window:alert', (text) => {
+//             expect(text).to.include('Your file has now been uploaded!')
+//         })
+
+
+//     })
+// })
