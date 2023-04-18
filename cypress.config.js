1eb56adbc278eee3731c624f8b60927a55de07ee
GopinathBhargav <vgopinathbhargav@outlook.com>
2023-04-18 12:04:45 +0400

    updated conig.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,31 +1,17 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-<<<<<<< HEAD
-=======
-
-    baseUrl: 'http://www.webdriveruniversity.com/Contact-Us/contactus.html',
-    "env": {
-      "url": "https://rahulshettyacademy.com/AutomationPractice/",
-      "test_url":"https://automationteststore.com/",
-      "cart_url":"https://rahulshettyacademy.com/seleniumPractise/#/"
+    baseUrl: "http://www.webdriveruniversity.com/Contact-Us/contactus.html",
+    env: {
+      url: "https://rahulshettyacademy.com/AutomationPractice/",
+      test_url: "https://automationteststore.com/",
+      cart_url: "https://rahulshettyacademy.com/seleniumPractise/#/",
     },
->>>>>>> 3a5d0415c527a4f92f64c9473884000c3c18b8ea
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-<<<<<<< HEAD
+      return require("./cypress/plugins/index.js")(on, config);
     },
   },
-})
-
-
-https://hooks.slack.com/services/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
-=======
-    }
-
-  },
-})
->>>>>>> 3a5d0415c527a4f92f64c9473884000c3c18b8ea
+});
