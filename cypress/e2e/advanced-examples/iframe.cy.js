545a6f7436e4f1d80ce73fc28d32db709f207979
GopinathBhargav <vgopinathbhargav@outlook.com>
2023-04-19 15:01:27 +0400

    uncommented all the spec files

--- cypress/e2e/advanced-examples/iframe.cy.js
+++ cypress/e2e/advanced-examples/iframe.cy.js
@@ -1,25 +1,18 @@
-// describe("verify iframes", () => {
+describe("verify iframes", () => {
+  it("verify by navigating to iframe and perform the actions on page", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
 
-//     it("verify by navigating to iframe and perform the actions on page", () => {
+    cy.get("#iframe").scrollIntoView().invoke("removeAttr", "target").click();
 
-//         cy.visit("http://www.webdriveruniversity.com/")
+    cy.get("#frame")
+      .its("0.contentDocument.body")
+      .then(cy.wrap)
+      .as("framePage");
 
-//         cy.get("#iframe")
-//             .scrollIntoView()
-//             .invoke('removeAttr', 'target')
-//             .click();
+    cy.get("@framePage")
+      .find("#button-find-out-more>b")
+      .should("have.text", "Find Out More!");
 
-//         cy.get("#frame")
-//             .its('0.contentDocument.body')
-//             .then(cy.wrap)
-//             .as("framePage")
-
-//         cy.get("@framePage")
-//             .find('#button-find-out-more>b').should('have.text', 'Find Out More!')
-
-//         cy.get("@framePage")
-//             .contains('a', 'Our Products')
-//             .click()
-//     })
-
-// })
+    cy.get("@framePage").contains("a", "Our Products").click();
+  });
+});
--- cypress/e2e/advanced-examples/select-file.cy.js
+++ cypress/e2e/advanced-examples/select-file.cy.js
@@ -1,27 +1,20 @@
-// describe("verify by selecting a file from project directory", () => {
+describe("verify by selecting a file from project directory", () => {
+  it("select the png file from project repository", () => {
+    cy.fixture("test_approach.jpeg", { encoding: null }).as("myFixture");
 
-//     it("select the png file from project repository", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
 
+    cy.get("#file-upload")
+      .scrollIntoView()
+      .invoke("removeAttr", "target")
+      .click();
 
-//         cy.fixture('test_approach.jpeg', { encoding: null }).as('myFixture')
+    cy.get("input#myFile").selectFile("@myFixture");
 
-//         cy.visit("http://www.webdriveruniversity.com/")
+    cy.get("#submit-button").click();
 
-//         cy.get("#file-upload")
-//             .scrollIntoView()
-//             .invoke('removeAttr', 'target')
-//             .click();
-
-//         cy.get("input#myFile")
-//             .selectFile('@myFixture')
-
-//         cy.get("#submit-button")
-//             .click()
-
-//         cy.on('window:alert', (text) => {
-//             expect(text).to.include('Your file has now been uploaded!')
-//         })
-
-
-//     })
-// })
+    cy.on("window:alert", (text) => {
+      expect(text).to.include("Your file has now been uploaded!");
+    });
+  });
+});
